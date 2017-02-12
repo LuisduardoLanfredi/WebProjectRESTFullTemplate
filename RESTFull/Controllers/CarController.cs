e92007f1dcfef450ae091768b7186be2d19f1141
Luís Eduardo Lanfredi <eduardo.lanfredi@gmail.com>
2017-02-12 13:35:58 -0200

    Remove files that should no be tracked by git

--- RESTFull/Controllers/CarController.cs
+++ RESTFull/Controllers/CarController.cs
@@ -23,7 +23,30 @@ namespace RESTFull.Controllers
         [Route("{id}")]
         public IHttpActionResult Get(int id)
         {
+            if (id == 0)
+                return BadRequest("Invalid parameter");
+
+            var result = _carRepository.Get(id);
+
+            if (result == null)
+                return NotFound();
+
             return Ok(_carRepository.Get(id));
+        }
+
+        [AcceptVerbs("POST")]
+        [Route("car")]
+        public IHttpActionResult Create(Car car)
+        {
+            if (car == null)
+                return BadRequest("Invalid parameter");
+
+            var result = _carRepository.Insert(car);
+
+            if (result == null)
+                return BadRequest();
+
+            return Ok(result);
         }
     }
 }
